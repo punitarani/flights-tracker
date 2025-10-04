0068c3d5ad9a53ff31c7c3a5928482737b193a4a
Punit Arani <punitsai36@gmail.com>
2025-10-04 10:25:50 -0700

    Install and setup tests with vitest

---
+++ vitest.config.ts
@@ -0,0 +1,32 @@
+/// <reference types="vitest" />
+
+import { dirname, resolve } from "node:path";
+import { fileURLToPath } from "node:url";
+import { defineConfig } from "vitest/config";
+
+const __filename = fileURLToPath(import.meta.url);
+const __dirname = dirname(__filename);
+
+export default defineConfig({
+  test: {
+    globals: true,
+    environment: "happy-dom",
+    coverage: {
+      provider: "v8",
+      reporter: ["text", "json", "html"],
+      exclude: [
+        "node_modules/",
+        "test/",
+        "**/*.d.ts",
+        "**/*.config.*",
+        "**/coverage/**",
+      ],
+    },
+    setupFiles: ["./src/test-setup.ts"],
+  },
+  resolve: {
+    alias: {
+      "@": resolve(__dirname, "./src"),
+    },
+  },
+});
