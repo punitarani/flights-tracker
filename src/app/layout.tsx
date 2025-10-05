4f0143fb5d4e1f5219c98668ea201f77829e3873
Punit Arani <punitsai36@gmail.com>
2025-10-05 10:20:25 -0700

    Install and set up nuqs

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
+import { NuqsAdapter } from "nuqs/adapters/next/app";
+
 import { TRPCProvider } from "@/lib/trpc/provider";
 
 const geistSans = Geist({
@@ -36,7 +38,9 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        <TRPCProvider>{children}</TRPCProvider>
+        <NuqsAdapter>
+          <TRPCProvider>{children}</TRPCProvider>
+        </NuqsAdapter>
       </body>
     </html>
   );
