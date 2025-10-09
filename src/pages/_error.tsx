24c869b49e182ad79c8e7d4ab00c31f2e79009ea
Punit Arani <punitsai36@gmail.com>
2025-10-08 23:49:46 -0700

    fix: error page props

--- src/pages/_error.tsx
+++ src/pages/_error.tsx
@@ -1,11 +1,13 @@
 import * as Sentry from "@sentry/nextjs";
-import NextError from "next/error";
+import NextError, { type ErrorProps } from "next/error";
 
-const CustomErrorComponent = (props) => {
+const CustomErrorComponent = (props: ErrorProps) => {
   return <NextError statusCode={props.statusCode} />;
 };
 
-CustomErrorComponent.getInitialProps = async (contextData) => {
+CustomErrorComponent.getInitialProps = async (
+  contextData: Parameters<typeof NextError.getInitialProps>[0],
+) => {
   // In case this is running in a serverless function, await this in order to give Sentry
   // time to send the error before the lambda exits
   await Sentry.captureUnderscoreErrorException(contextData);
